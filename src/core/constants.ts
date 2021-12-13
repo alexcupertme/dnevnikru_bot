c05eeb9e98c464fd2a5b7c5fbf600920fe745b7b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-12-13 22:43:13 +0300

    Add constants file

---
+++ src/core/constants.ts
@@ -0,0 +1,19 @@
+export const constants = {
+	apiUrl: "https://api.dnevnik.ru/v2.0",
+	baseUrl: "https://dnevnik.ru/",
+	loginUrl: "https://login.dnevnik.ru/login/",
+	accessToken: "tJAj6mELh2ZRo9WrNSatWmXxpxIjvkGf",
+	personId: 1000010493564,
+	eduGroup: 1847814077665489232,
+	telegramToken: "2110027437:AAE6iilfaKDRszMNr4gr6qqbkFjMNxAbJEk",
+	redis: {
+		host: "dnevnikru-6871.redis.dbs.scalingo.com:36251",
+		password: "tmQQG67oqkExCrPrH9hp",
+	},
+	mongodb: {
+		username: "",
+		password: "",
+		host: "127.0.0.1",
+		db_name: "dnevnikru",
+	},
+};
