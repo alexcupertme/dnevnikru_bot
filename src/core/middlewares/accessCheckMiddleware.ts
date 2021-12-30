d3f1fb86f69a9b7b87421c8e1c6f16ca4368a2cd
vzlomed <sahahb99@mail.ru>
2021-12-30 22:19:38 +0300

    Fix access check middleware behavior on dnevnik ru errors

--- src/core/middlewares/accessCheckMiddleware.ts
+++ src/core/middlewares/accessCheckMiddleware.ts
@@ -1,16 +1,22 @@
 import { MyContext } from "../services/bot.service";
-import { Context } from "../../api";
+import { CheckAccess } from "../../api";
+import { InlineKeyboard } from "grammy";
 
 export function accessCheckMiddleware(accessToken: string) {
 	return async function (ctx: MyContext, next: any) {
 		const cmd = ctx.update.message?.text?.toLowerCase();
-		const context = new Context(accessToken);
-		if (cmd != "/start" && cmd != "/auth") {
-			const res = await context.me();
+		const callbackMsg = ctx.update.callback_query?.data?.toLowerCase();
+		const context = new CheckAccess(accessToken);
+		if (cmd != "/start" && cmd != "/auth" && callbackMsg != "help" && callbackMsg != "menu" && callbackMsg != "about" && callbackMsg != "start" && callbackMsg != "auth") {
+			const res = await context.default();
 			if (res.response.statusCode == 401) {
-				await ctx.reply("⚠️ Ваш аккаунт не авторизован! Авторизуйтесь через команду /auth");
+				await ctx.editMessageText("⚠️ Ваш аккаунт не авторизован! Авторизуйтесь через команду /auth", {
+					reply_markup: new InlineKeyboard().text("Назад", "start").text("Авторизоваться", "auth").row(),
+				});
 			} else if (res.response.statusCode == 500) {
-				await ctx.reply("😔 Нам очень жаль, но сервера Dnevnik.ru сейчас находятся на тех. обслуживании. Попробуйте выполнить команду через некоторое время (15 мин)");
+				await ctx.editMessageText("😔 Нам очень жаль, но сервера Dnevnik.ru сейчас находятся на тех. обслуживании. Попробуйте выполнить команду через некоторое время (15 мин)", {
+					reply_markup: new InlineKeyboard().text("Назад", "start"),
+				});
 			} else return await next();
 		} else return await next();
 	};
